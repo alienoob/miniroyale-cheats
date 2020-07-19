3734302c4cca07659b6423f5020cd48f3d923430
alienoob <alienoob@protonmail.com>
2020-07-19 13:38:26 +0300

    Refactor lines.js

--- lines.js
+++ lines.js
@@ -1,12 +1,9 @@
-const onPlayerCreate = PlayerManager.prototype.onPlayerCreate;
 const onMovementUpdateAnimation = Movement.prototype.updateAnimation;
 
 const HEAD_OFFSET = 0.4;
 const RENDER_LINE_STEP = 0.001;
 const RENDER_LINE_WIDTH = 0.03;
 const RED_COLOR = new pc.Color(1, 0, 0);
-
-let enemies = null;
 
 function renderLine(application, from, to, color) {
   for (let i = 0; i < RENDER_LINE_WIDTH; i += RENDER_LINE_STEP) {
@@ -17,20 +14,11 @@ function renderLine(application, from, to, color) {
   }
 }
 
-PlayerManager.prototype.onPlayerCreate = function () {
-  onPlayerCreate.apply(this, arguments);
-
-  enemies = this.players;
-};
-
 Movement.prototype.updateAnimation = function () {
   onMovementUpdateAnimation.apply(this, arguments);
 
-  if (!enemies) {
-    return;
-  }
-
   const position = this.entity.getPosition().clone();
+  const enemies = this.playerManager.script.playerManager.players;
 
   for (const enemy of enemies) {
     renderLine(this.app, enemy.localPosition, position, RED_COLOR);
